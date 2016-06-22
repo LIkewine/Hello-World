9b5115e1dc7b64f99c0c5f9383d430df357f03be
yuhai <yuhai384@gmail.com>
2016-06-22 22:54:23 +0800

    Add hello_world  by .c

---
+++ hello_world.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello_world.c
+	> Author:yuhai 
+	> Mail:yuhai384@gmail.com 
+	> Created Time: 2016年06月22日 星期三 22时51分37秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+    printf("hello world\n");
+    return 0;
+}
